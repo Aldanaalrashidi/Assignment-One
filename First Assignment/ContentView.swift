fa546e1f3b8a7e7cde240f38f4746e45e4478b26
61-الدانة سعد الرشيدي <aldanaalrashidi0@gmail.com>
2020-08-19 20:56:14 +0300

    Initial Commit

---
+++ First Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First Assignment
+//
+//  Created by tahani alrashidi on 8/19/20.
+//  Copyright © 2020 Aldana Al-Rashidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
