5080741be947f790b1b3f75f2f7274f1ca07706c
61-الدانة سعد الرشيدي <aldanaalrashidi0@gmail.com>
2020-08-21 21:52:36 +0300

    added modifiers

--- First Assignment/Calculator.swift
+++ First Assignment/Calculator.swift
@@ -14,7 +14,6 @@ struct Calculator: View {
     let column3 = ["%", "9", "6", "3"]
     let column4 = ["÷", "X", "-", "+"]
     
-    
     var body: some View {
         ZStack(alignment: .bottom){
             Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
@@ -31,27 +30,18 @@ struct Calculator: View {
                     VStack(spacing: 11){
                         ForEach(self.column1, id: \.self){
                             button in Text(button)
-                                .font(.system(size: 32))
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .frame(width: 85, height: 85)
-                                .background(Color(#colorLiteral(red: 0.556738019, green: 0.5565260053, blue: 0.577188611, alpha: 1)))
-                                .cornerRadius(40)}}
+                                .modifier(CalculatorModifier())
+                        }}
                     VStack(spacing: 11){
                         ForEach(self.column2, id: \.self){
                             button in Text(button)
-                                .font(.system(size: 32))
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .frame(width: 85, height: 85)
-                                .background(Color(#colorLiteral(red: 0.556738019, green: 0.5565260053, blue: 0.577188611, alpha: 1)))
-                                .cornerRadius(40)}}
+                                .modifier(CalculatorModifier())
+                        }}
                     VStack(spacing: 11){
                         ForEach(self.column3, id: \.self){
                             button in Text(button)
-                                .font(.system(size: 32))
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .frame(width: 85, height: 85)
-                                .background(Color(#colorLiteral(red: 0.556738019, green: 0.5565260053, blue: 0.577188611, alpha: 1)))
-                                .cornerRadius(40)}}
+                                .modifier(CalculatorModifier())
+                        }}
                     VStack(spacing: 11){
                         ForEach(self.column4, id: \.self){
                             button in Text(button)
@@ -69,12 +59,8 @@ struct Calculator: View {
                         .background(Color(#colorLiteral(red: 0.556738019, green: 0.5565260053, blue: 0.577188611, alpha: 1)))
                         .cornerRadius(40)
                     Text(".")
-                        .font(.system(size: 32))
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                        .frame(width: 85, height: 85)
-                        .background(Color(#colorLiteral(red: 0.556738019, green: 0.5565260053, blue: 0.577188611, alpha: 1)))
-                        .cornerRadius(40)
-                    Text("-")
+                        .modifier(CalculatorModifier())
+                    Text("=")
                         .font(.system(size: 32))
                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                         .frame(width: 85, height: 85)
@@ -89,6 +75,17 @@ struct Calculator: View {
 struct Calculator_Previews: PreviewProvider {
     static var previews: some View {
         Calculator()
+    }
+}
+
+struct CalculatorModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 32))
+            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+            .frame(width: 85, height: 85)
+            .background(Color(#colorLiteral(red: 0.556738019, green: 0.5565260053, blue: 0.577188611, alpha: 1)))
+            .cornerRadius(40)
     }
 }
 
